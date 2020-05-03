bddd8290da94114feb489c133fa59a3fdabe073a
CARICOM12 <0190a@ukr.net>
2020-05-03 15:26:01 +0300

    Add files via upload

---
+++ BoolUsingPractice/BoolUsingPractice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BoolUsingPractice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("10 > 9 is " + (10 > 9));
+            Console.WriteLine("100 < 50 is " + (100 < 50));
+            Console.WriteLine("100 <= 100 is " + (100 <= 100));
+            Console.WriteLine("100 >= 200 is " + (100 >= 200));
+            Console.WriteLine("100 = 30 is " + (100 == 30));
+        }
+    }
+}
